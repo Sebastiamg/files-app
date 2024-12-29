9371b3c8e4ddffd8c77158834bff80d4f94090c4
Sebastiamg <sebasortiz112004@gmail.com>
2024-12-28 19:06:13 -0500

    fallo Weak

--- services/pdf.service.ts
+++ services/pdf.service.ts
@@ -3,10 +3,8 @@ import * as Print from "expo-print";
 
 export async function sharePDF(html: string) {
   if (html.length > 10) {
-    const uri = await Print.selectPrinterAsync();
-
-    // const { uri } = await Print.printToFileAsync({ html });
-    // await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
+    const { uri } = await Print.printToFileAsync({ html });
+    await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
   } else {
     return "No HTML data";
   }
